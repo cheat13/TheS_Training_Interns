d5257a12934ce5d3257f978c5e0121137f418c75
unravel07 <mick_ben10@hotmail.com>
2019-02-03 09:55:40 +0700

    mick

--- TrainingIntern/src/pages/about/about.ts
+++ TrainingIntern/src/pages/about/about.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  public text: string;
+  public number: number;
+  public times = [];
+  public keep: any;
 
+  constructor(public navCtrl: NavController) {
+    this.times = [1, 2, 3, 4, 5, 6];
   }
 
+  test() {
+    this.navCtrl.push("TestPage");
+  }
 }
